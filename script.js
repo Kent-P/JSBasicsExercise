7b56862c9fd6952ca2321dd0fbfedccc4b528ec5
Kenton Poynter <kenton.poynter@stu.barren.kyschools.us>
2024-01-12 10:12:48 -0600

    Completed

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log(4)
+// Numbers, Strings, Booleans, Null, Undefined
+// + , - , * , / , % , **
+
+console.log(5 % 2)
+console.log(5 % 4)
+
+console.log('I\'M MARY POPPINS\, Y\'ALL!')
+console.log("Hawk" + "eye")
+
+console.log("Kenton".length + "Poynter".length)
+console.log("Avengers: Endgame"[16])
+
+console.log("\\")
+
+console.log(5 ** 4 - 9)
+
+console.log("I".length - "Am".length + "Iron".length % "Man".length)
+
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("OKOYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[0])
+console.log("DOCTOR STRANGE"[13])
+console.log("THOR"[3])
+console.log("STAR-LORD"[0])
+
+m=25
+c=4
+u= m + c
